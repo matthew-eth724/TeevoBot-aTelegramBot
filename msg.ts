e85a070522d94b97932d380330788fc1f864e0be
matthew-eth724 <tokoniayakazi.eth@gmail.com>
2025-06-01 09:41:37 +0100

    Update msg.ts

--- msg.ts
+++ msg.ts
@@ -1,3 +1,3 @@
-export const teevos: string[] = ["./teevos/Teevo_May2025.pdf"]
+export const teevos: string[] = ["./teevos/Teevo_May2025.pdf", "./teevos/Teevo_June2025.pdf"]
 
 export const months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
