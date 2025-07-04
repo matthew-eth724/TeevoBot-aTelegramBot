823b57120679b36f825921005c350e6f2d2fab9a
IRENE UFUOMA AYAKAZI <fuosirene2014@gmail.com>
2025-07-04 18:22:11 +0100

    added july

--- msg.js
+++ msg.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.months = exports.teevos = void 0;
-exports.teevos = ["./teevos/Teevo_May2025.pdf", "./teevos/Teevo_June2025.pdf"];
+exports.teevos = ["./teevos/Teevo_May2025.pdf", "./teevos/Teevo_June2025.pdf", "./teevos/Teevo_July2025.pdf"];
 exports.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
